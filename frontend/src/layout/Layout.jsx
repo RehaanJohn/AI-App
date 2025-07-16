f2aaa48455fea86a308103cbcc0d21f255cc07ba
Rehaan <rehaanrafael.john@gmail.com>
2025-07-16 23:21:36 +0530

    updated-key

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,8 +1,23 @@
 import "react"
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+import { Outlet, Link, Navigate } from "react-router-dom";
 
 export function Layout() {
-    return 
-    <>
-    </>
+    return <div className ="app-layout">
+        <header>
+            <div>
+                <h1>
+                    <nav>
+                        <SignedIn>
+                            <Link to="/">Home</Link>
+                            <Link to="/history">History</Link>
+                            <UserButton />
+                        </SignedIn>
+                    </nav>
+                </h1>
+            </div>
+        </header>
+        
+    </div>
 }
 
